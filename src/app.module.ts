5ff61c3690a8ca2f96a37ba531a9ebb7ca74fd93
leoayresec <leoayresec@gmail.com>
2025-07-16 17:06:57 -0300

    Criação do módulo auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { CharacterModule } from './character/character.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CharacterModule, UserModule],
+  imports: [CharacterModule, UserModule, AuthModule],
   controllers: [],
   providers: [],
 })
